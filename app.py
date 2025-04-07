290bb3ee93c2eba9bdb8f8217f14117ac001a441
Jonatan Boshoff <140970186+boshify@users.noreply.github.com>
2025-04-07 13:15:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer
 
 # Initialize the model for title similarity
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
 
 # Initialize session state for data and processing status
 if 'data' not in st.session_state:
